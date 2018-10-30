f6851586981fc008e0420b4514d03569222fb5f9
Florent Pastor <florent.pastor@ostmodern.co.uk>
2018-10-30 14:03:28 +0100

    Deleted the post object; made unittest actually unittests

--- tests.py
+++ tests.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 from requests_mock import Mocker
 
 from server import (
-    tumblr_session, posts_count, random_post, post_url, random_long_post, Post,
-    post_by_id, nth_previous_posts)
+    tumblr_session, posts_count, random_post, post_url, random_long_post,
+    parse_post, post_by_id, nth_previous_posts)
 
 TUMBLR_URL_POST = "https://api.tumblr.com/v2/blog/www.humansofnewyork.com/posts/photo/"
 TUMBLR_URL_BLOG = "https://api.tumblr.com/v2/blog/www.humansofnewyork.com/info"
@@ -30,6 +30,14 @@ TUMBLR_RESPONSE_POST = """{
         ]
     }
 }"""
+
+
+TUMBLR_POST = {
+    "slug": "a-slug",
+    "post_url": "http://www.humansofnewyork.com/url",
+    "caption": "<p>a_caption</p>",
+    "timestamp": 1540424820
+}
 
 
 TUMBLR_RESPONSE_THREE_POSTS = """{
@@ -58,6 +66,42 @@ TUMBLR_RESPONSE_THREE_POSTS = """{
 }"""
 
 
+TUMBLR_THREE_POSTS_LAST_POST = {
+    "slug": "a-slug",
+    "post_url": "http://www.humansofnewyork.com/url",
+    "caption": "<p>a_caption_last</p>",
+    "timestamp": 1540424820
+}
+
+
+def make_test_post(post_url="http://www.humansofnewyork.com/url",
+                   slug="a-slug",
+                   caption="<p>a_caption</p>",
+                   timestamp=1540424820):
+    return {
+        "post_url": post_url,
+        "caption": caption,
+        "timestamp": timestamp,
+        "slug": slug
+    }
+
+
+def test_parse_post__parses_post_url():
+    assert parse_post(TUMBLR_POST)["post_url"] == "http://www.humansofnewyork.com/url"  # noqa
+
+
+def test_parse_post__parses_slug():
+    assert parse_post(TUMBLR_POST)["slug"] == "a-slug"
+
+
+def test_parse_post__parses_caption():
+    assert parse_post(TUMBLR_POST)["caption"] == "<p>a_caption</p>"
+
+
+def test_parse_post__parses_timestamp():
+    assert parse_post(TUMBLR_POST)["timestamp"] == 1540424820
+
+
 def test_tumblr_session_has_correct_key():
     with patch('server.os.environ', {"TUMBLR_API_KEY": "key"}):
         session = tumblr_session()
@@ -72,14 +116,13 @@ def test_post_count__returns_post():
     assert count == 7260
 
 
-def test_random_post__returns_correct():
-    with Mocker() as m:
+def test_random_post__passes_correct_post():
+    parse_post_patch = patch("server.parse_post", side_effect=make_test_post)
+    with Mocker() as m, parse_post_patch as parse_post_mock:
         m.get(TUMBLR_URL_POST, text=TUMBLR_RESPONSE_POST)
-        post = random_post(1138)
+        random_post(1138)
 
-    assert post.post_url == "http://www.humansofnewyork.com/url"
-    assert post.caption == "<p>a_caption</p>"
-    assert post.timestamp == 1540424820
+    parse_post_mock.assert_called_with(TUMBLR_POST)
 
 
 def test_random_post__selects_article():
@@ -92,11 +135,11 @@ def test_random_post__selects_article():
 
 
 def test_post_url():
-    assert post_url(Post("url", "caption", 5555)) == "url"
+    assert post_url(make_test_post(post_url="url")) == "url"
 
 
 def test_random_long_post__first_post_is_long_enough():
-    post = Post("url", ["c" for i in range(501)], 5555)
+    post = make_test_post(caption=["c" for i in range(501)])
 
     with patch("server.random_post", side_effect=[post]):
         returned_post = random_long_post(1138)
@@ -106,9 +149,9 @@ def test_random_long_post__first_post_is_long_enough():
 
 def test_random_long_post__third_post_is_long_enough():
     posts = [
-        Post("url", ["c" for i in range(300)], 555),
-        Post("url", ["c" for i in range(100)], 555),
-        Post("url", ["c" for i in range(501)], 555)
+        make_test_post(caption=["c" for i in range(300)]),
+        make_test_post(caption=["c" for i in range(100)]),
+        make_test_post(caption=["c" for i in range(501)])
     ]
 
     with patch("server.random_post", side_effect=posts):
@@ -117,14 +160,13 @@ def test_random_long_post__third_post_is_long_enough():
     assert returned_post == posts[2]
 
 
-def test_post_by_id__return_post():
-    with Mocker() as m:
+def test_post_by_id__passes_correct_post():
+    parse_post_patch = patch("server.parse_post", side_effect=make_test_post)
+    with Mocker() as m, parse_post_patch as parse_post_mock:
         m.get(TUMBLR_URL_POST, text=TUMBLR_RESPONSE_POST)
-        post = post_by_id(4815162342)
+        post_by_id(4815162342)
 
-    assert post.post_url == "http://www.humansofnewyork.com/url"
-    assert post.caption == "<p>a_caption</p>"
-    assert post.timestamp == 1540424820
+    parse_post_mock.assert_called_with(TUMBLR_POST)
 
 
 def test_post_by_id__return_none_if_do_not_exists():
@@ -146,21 +188,16 @@ def test_post_by_id__correct_argument_passed():
 def test_nth_previous_posts__correct_arguments_passed():
     with Mocker() as m:
         m.get(TUMBLR_URL_POST, text=TUMBLR_RESPONSE_THREE_POSTS)
-        nth_previous_posts(Post("url", "caption", 5555), 3)
+        nth_previous_posts(make_test_post(timestamp=5555), 3)
 
     assert m.request_history[0].qs["before"] == ["5554"]
     assert m.request_history[0].qs["limit"] == ['3']
 
 
-def test_nth_previous_posts__return_correct_post():
-    with Mocker() as m:
+def test_nth_previous_posts__passes_correct_post():
+    parse_post_patch = patch("server.parse_post", side_effect=make_test_post)
+    with Mocker() as m, parse_post_patch as parse_post_mock:
         m.get(TUMBLR_URL_POST, text=TUMBLR_RESPONSE_THREE_POSTS)
-        post = nth_previous_posts(Post("url", "caption", 5555), 3)
+        nth_previous_posts(make_test_post(timestamp=5555), 3)
 
-    assert post.post_url == "http://www.humansofnewyork.com/url"
-    assert post.caption == "<p>a_caption_last</p>"
-    assert post.timestamp == 1540424820
-
-
-def test_post_number_in_series():
-    pass
+    parse_post_mock.assert_called_with(TUMBLR_THREE_POSTS_LAST_POST)
